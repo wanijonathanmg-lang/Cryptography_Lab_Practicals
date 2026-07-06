ad45c457726a6e16aee6bf9a09e964461be80577
Jonathan Wani Mogga <wanijonathanmg@gmail.com>
2026-07-06 11:03:54 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,8 @@
+# A program to find if the entered number is divisible by 7 using python.
+
+no=int(input("ENter a number : "))
+
+if(no%7!=0):
+    print(f"The number {no} is not divisible by 7.")
+else:
+    print(f"The number {no} is divisible.")
